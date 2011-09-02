4cf94ac438b17682dd5ce9829bd64ca547ff3397
Vibhuti <vinti.uiet@gmail.com>
2011-09-02 01:52:29 -0700

    Editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    #raise params[:user].inspect
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to user_path(@user), :flash =>{:success => "Welcome to the Sample App!"}
+      redirect_to @user, :flash =>{:success => "Welcome to the Sample App!"}
     else
       @title = "Sign up"
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash =>{:success => "Profile updated"}
+    else
+      @title = "Edit user"
+      render 'edit'
     end
   end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -50,31 +50,37 @@ describe "LayoutLinks" do
     it "should have a sign in link" do
       visit root_path
       response.should have_selector("a", :href => signin_path,
-                                         :content => "Sign in")
+                                    :content => "Sign in")
     end
 
-  describe "when signed in" do
+    describe "when signed in" do
 
-    before (:each) do
-      @user = Factory(:user)
-      visit signin_path
-      fill_in :email, :with     => @user.email
-      fill_in :password, :with   => @user.password
-      click_button
+      before (:each) do
+        @user = Factory(:user)
+        visit signin_path
+        fill_in :email, :with => @user.email
+        fill_in :password, :with => @user.password
+        click_button
+      end
+
+      it "should have a signout link" do
+        visit root_path
+        response.should have_selector("a", :href => signout_path,
+                                      :content =>"Sign out")
+      end
+
+      it "should have a profile link" do
+        visit root_path
+        response.should have_selector("a", :href => user_path(@user),
+                                      :content =>"Profile")
+
+      end
+
+      it "should have a settings link" do
+        visit root_path
+        response.should have_selector("a", :href => edit_user_path(@user),
+                                      :content => "Settings")
+      end
     end
-
-    it "should have a signout link" do
-      visit root_path
-      response.should have_selector("a", :href => signout_path,
-                                          :content =>"Sign out")
-    end
-
-    it "should have a profile link" do
-      visit root_path
-      response.should have_selector("a", :href => user_path(@user),
-                                          :content =>"Profile")
-
-    end
-  end
   end
 end
